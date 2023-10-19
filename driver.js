bf1d1bbebfc752e14be9d3b12b663820ae05d5a9
Arne Hilao <arne.hilao@gmail.com>
2023-10-19 11:49:47 -0600

    Added skeleton of functions and browser sections

---
+++ driver.js
@@ -0,0 +1,20 @@
+//IMPORTANT FUNCTIONS
+
+//Check for Checkmate
+
+//Check for Stalemate
+
+//Check for Legal Moves
+
+//Switch Active Player
+
+//Add Piece to Point Advantage
+
+//Add Move to Move List
+
+//Make Move
+//      -Check Legal Move
+//      -Add Move to Move List
+//      -Check for Checkmate
+//      -Check for Stalemate
+//      -Switch Active Player
