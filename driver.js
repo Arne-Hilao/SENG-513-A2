88b8848d4a07e13fbdb5fe5ce968f3eb93f65f94
Arne-Hilao <arne.hilao@gmail.com>
2023-10-20 21:59:23 -0600

    expanded comments for the driver code to prepare skeleton

--- driver.js
+++ driver.js
@@ -4,23 +4,38 @@
 /* Name: Arne Jarred Michael Hilao */
 /* UCID: 30069573 */
 
-//IMPORTANT FUNCTIONS
+//Check for Promotion
 
-//Check for Checkmate
-
-//Check for Stalemate
-
-//Check for Legal Moves
-
-//Switch Active Player
-
-//Add Piece to Point Advantage
-
-//Add Move to Move List
-
-//Make Move
+//MAKE MOVE
+//      -Important functions to implement for Make move
 //      -Check Legal Move
+//          -Check on Move
+//          -Reveal moves on right click
+//      -Check Capture
+//          -Check on move to remove captured piece
 //      -Add Move to Move List
+//          -Add to li if bool:white is true. Add new li if false
 //      -Check for Checkmate
+//          -If checkmate, add 1 to active player score
+//          -Switch Player Sides (Player 1 becomes player 2)
+//              -Flip bool: active_player. so wins get added to the correct score
 //      -Check for Stalemate
+//          -If stalemate, add 0.5 to both scores
+//              -Stalemate happens when a king can't move, but is not in check
+//      -Check for Draw
+//          -Happens when there are only two kings
+//          -Also happens when there is only 1 bishop or 1 knight on one side
+//          -Happens when 50 kign moves in a row happen (not implemented)
+//          -Happens when a position is reached 3 times (not implemented)
+//          -Happens when the draw button is pressed and confirmed.
 //      -Switch Active Player
+//          -If all checks are passed, switch active player
+
+//TIMER
+//      -Check for end.
+//          -If time is finished, consider win for non-active player
+
+/**
+ * Game flow:
+ * 
+ */
